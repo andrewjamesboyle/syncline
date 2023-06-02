28c4a196ace1d2113fc65646bd07a3758f529872
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-06-02 13:18:05 -0700

    tailwind css install

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,18 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>
+        Syncline Software Solutions is a custom web application and digital
+        marketing studio dedicated to helping small businesses in the Columbia
+        River Gorge leverage technology and generative AI tools to automate
+        aspects of their business.
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,11 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      Leverage generative AI technology to streamline your business and spend
+      more time riding.
     </>
   )
 }
