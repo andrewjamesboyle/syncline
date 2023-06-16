70e4bd4a1213e4a59eab0010c73e597f2ea033d9
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-06-16 16:39:34 -0700

    updates to about section

--- web/src/components/Contact/Contact.tsx
+++ web/src/components/Contact/Contact.tsx
@@ -1,6 +1,25 @@
 import { BuildingOffice2Icon, EnvelopeIcon } from '@heroicons/react/24/outline'
 
+import {
+  FieldError,
+  Form,
+  Submit,
+  SubmitHandler,
+  TextAreaField,
+  TextField,
+  useForm,
+} from '@redwoodjs/forms'
+
+interface FormValues {
+  input: string
+}
+
 const Contact = () => {
+  const formMethods = useForm({ mode: 'onBlur' })
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data)
+  }
   return (
     <section id="Contact">
       <div className="relative isolate bg-white">
@@ -80,16 +99,19 @@ const Contact = () => {
                   <dd>
                     <a
                       className="hover:text-gray-900"
-                      href="mailto:andrewboylecodes@gmail.com"
+                      href="mailto:info@synclinesoftwaresolutions.com"
                     >
-                      andrewboylecodes@gmail.com
+                      info@synclinesoftwaresolutions.com
                     </a>
                   </dd>
                 </div>
               </dl>
             </div>
           </div>
-          <form
+          <Form
+            onSubmit={onSubmit}
+            config={{ mode: 'onBlur' }}
+            formMethods={formMethods}
             action="#"
             method="POST"
             className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
@@ -104,13 +126,14 @@ const Contact = () => {
                     First name
                   </label>
                   <div className="mt-2.5">
-                    <input
-                      type="text"
+                    <TextField
                       name="first-name"
                       id="first-name"
                       autoComplete="given-name"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      validation={{ required: true }}
                     />
+                    <FieldError name="first-name" />
                   </div>
                 </div>
                 <div>
@@ -121,13 +144,14 @@ const Contact = () => {
                     Last name
                   </label>
                   <div className="mt-2.5">
-                    <input
-                      type="text"
+                    <TextField
                       name="last-name"
                       id="last-name"
                       autoComplete="family-name"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      validation={{ required: true }}
                     />
+                    <FieldError name="last-name" />
                   </div>
                 </div>
                 <div className="sm:col-span-2">
@@ -138,13 +162,20 @@ const Contact = () => {
                     Email
                   </label>
                   <div className="mt-2.5">
-                    <input
-                      type="email"
+                    <TextField
                       name="email"
                       id="email"
                       autoComplete="email"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      validation={{
+                        required: true,
+                        pattern: {
+                          value: /^[^@]+@[^.]+\..+$/,
+                          message: 'Please enter a valid email address',
+                        },
+                      }}
                     />
+                    <FieldError name="email" />
                   </div>
                 </div>
                 <div className="sm:col-span-2">
@@ -155,13 +186,14 @@ const Contact = () => {
                     Phone number
                   </label>
                   <div className="mt-2.5">
-                    <input
-                      type="tel"
+                    <TextField
                       name="phone-number"
                       id="phone-number"
                       autoComplete="tel"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      validation={{ required: true }}
                     />
+                    <FieldError name="phone-number" />
                   </div>
                 </div>
                 <div className="sm:col-span-2">
@@ -172,26 +204,24 @@ const Contact = () => {
                     Message
                   </label>
                   <div className="mt-2.5">
-                    <textarea
+                    <TextAreaField
                       name="message"
                       id="message"
-                      rows={4}
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       defaultValue={''}
+                      validation={{ required: true }}
                     />
+                    <FieldError name="message" />
                   </div>
                 </div>
               </div>
               <div className="mt-8 flex justify-end">
-                <button
-                  type="submit"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
+                <Submit className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                   Send message
-                </button>
+                </Submit>
               </div>
             </div>
-          </form>
+          </Form>
         </div>
       </div>
     </section>
