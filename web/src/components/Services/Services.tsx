40449176f532e60485d65667ef712a48dfe26dce
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-06-21 10:11:25 -0700

    get more info updates

--- web/src/components/Services/Services.tsx
+++ web/src/components/Services/Services.tsx
@@ -92,7 +92,7 @@ const Services = () => {
                         }}
                         className="text-sm font-semibold leading-6 text-blue-400"
                       >
-                        Contact <span aria-hidden="true"></span>
+                        Get More Info <span aria-hidden="true"></span>
                       </button>
                     </p>
                   </dd>
